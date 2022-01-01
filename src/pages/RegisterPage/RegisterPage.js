8d05160948432ed3f8fe95a15d6cf3f23c104869
Chun-gu <chungu.yi92@gmail.com>
2022-01-02 00:00:13 +0900

    ✨ register 기능 구현

--- src/pages/RegisterPage/RegisterPage.js
+++ src/pages/RegisterPage/RegisterPage.js
@@ -1,13 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import Clayful from "clayful/client-js";
 
 export const RegisterPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  var Customer = Clayful.Customer;
+
+  var payload = {
+    email,
+    password,
+  };
+console.log(payload)
+  Customer.createMe(payload, function (err, result) {
+    if (err) {
+      // Error case
+      console.log(err.code);
+    }
+
+    var data = result.data;
+
+    console.log(data);
+  });
+
   return (
     <div className="auth-wrapper">
       <h1>회원가입</h1>
-      <form>
-        <input placeholder="Apple Id" type="email" name="email" value="" />
-        <input placeholder="암호" type="password" name="password" value="" />
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={handleEmailChange}
+          placeholder="Apple Id"
+          type="email"
+          name="email"
+          value={email}
+        />
+        <input
+          onChange={handlePasswordChange}
+          placeholder="암호"
+          type="password"
+          name="password"
+          value={password}
+        />
         <button type="submit">회원가입</button>
         <Link to="login" style={{ color: "grey", textDecoration: "none" }}>
           이미 Apple Id가 있다면? 지금 로그인
